0f8048d0cd3bfc512d324226e4d49774e97702e8
Bryan F.R <bryan.ferreras@gmail.com>
2020-06-09 17:29:48 +0200

    Implémentation du SwipeRefreshLayout pour NeighbourFragment

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourFragment.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -34,6 +35,7 @@ public class NeighbourFragment extends Fragment {
     private List<Neighbour> mNeighbours;
     private RecyclerView mRecyclerView;
     public static final String KEY_USER_ID = "KEY_USER_ID";
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
 
     /**
@@ -57,11 +59,23 @@ public class NeighbourFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_neighbour_list, container, false);
         Context context = view.getContext();
-        mRecyclerView = (RecyclerView) view;
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.list_neighbours);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
         mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.fragment_neighbourlist_swipe_container);
+        this.configureSwipeRefreshLayout();
         this.configureOnClickRecyclerView();
         return view;
+    }
+
+    private void configureSwipeRefreshLayout() {
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                initList();
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
     }
 
     /**
